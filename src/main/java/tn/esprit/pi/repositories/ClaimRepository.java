c9a4fef2ed54bf2c85e6cfe9b7f15973ba96d392
Roua_Boussetta <60694832+RouaBoussetta@users.noreply.github.com>
2021-02-24 12:47:56 +0100

    Metier claim

--- src/main/java/tn/esprit/pi/repositories/ClaimRepository.java
+++ src/main/java/tn/esprit/pi/repositories/ClaimRepository.java
@@ -2,16 +2,27 @@ package tn.esprit.pi.repository;
 
 
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import tn.esprit.pi.entities.Claim;
+import tn.esprit.pi.entities.ClaimCategory;
+
 
 @Repository
 public interface ClaimRepository extends CrudRepository<Claim,Integer> {
 
 	
-
+	@Query("SELECT c FROM Claim c WHERE c.category =:category")
+	public List<Claim> getClaimByCategory(@Param("category")ClaimCategory claimCat);
+	
+	
+	
+	@Query("SELECT c FROM Claim c WHERE c.kindergarden.name =:name")
+	public List<Claim> getClaimByKindergarden(@Param("name")String k);
 
 }
