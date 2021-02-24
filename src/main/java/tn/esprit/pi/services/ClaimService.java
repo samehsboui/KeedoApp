4df8ff3cd91f2d5441eced95217a47622f6a120a
Roua_Boussetta <60694832+RouaBoussetta@users.noreply.github.com>
2021-02-24 12:46:11 +0100

    crud + Metier claim

--- src/main/java/tn/esprit/pi/services/ClaimService.java
+++ src/main/java/tn/esprit/pi/services/ClaimService.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import tn.esprit.pi.entities.Claim;
-
- 
-
+import tn.esprit.pi.entities.ClaimCategory;
 import tn.esprit.pi.repository.ClaimRepository;
 
 
@@ -68,10 +66,30 @@ public class ClaimService implements IClaimService {
 		return claims.size();
 	}
 
+	@Override
+	public List<Claim> getClaimByCategory(ClaimCategory cc) {
+		// TODO Auto-generated method stub
+		return cr.getClaimByCategory(cc);
+	}
+
+	@Override
+	public List<Claim> getClaimByKindergarden(String k) {
+		// TODO Auto-generated method stub
+		return cr.getClaimByKindergarden(k);
+	}
+
+	
+	@Override
+	public int CountClaimByKindergarden(String k) {
+		// TODO Auto-generated method stub
+		
+		List <Claim> claims=(List<Claim>) cr.getClaimByKindergarden(k);
+		return claims.size();
+		
+	}
 	
 	
-	
-	
+
 	
 
 	
